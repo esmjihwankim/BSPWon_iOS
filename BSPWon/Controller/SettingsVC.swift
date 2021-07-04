9ecfd214c7b27c8e8d6b7d3583be4ef4323ce0aa
Jihwan Kim <raptorwoo@gmail.com>
2021-07-04 17:38:21 +0900

    add textfield responder logic

--- BSPWon/Controller/SettingsVC.swift
+++ BSPWon/Controller/SettingsVC.swift
@@ -14,15 +14,35 @@ class SettingVC : UIViewController
     @IBOutlet weak var topBannerView: UIView!
     @IBOutlet weak var yMaxTextField: UITextField!
     @IBOutlet weak var yMinTextField: UITextField!
+    @IBOutlet weak var applyButton: UIButton!
+    
     
     override func viewDidLoad()
     {
-        
+        setUI()
     }
     
     func setUI()
     {
-        
+        self.topBannerView.layer.cornerRadius = 10.0
+        self.topBannerView.layer.backgroundColor = UIColor.black.cgColor
+        self.applyButton.layer.cornerRadius = 5.0
+        self.applyButton.layer.backgroundColor = UIColor.systemBlue.cgColor
+        self.applyButton.tintColor = UIColor.white
+        yMaxTextField.keyboardType = UIKeyboardType.numberPad
+        yMinTextField.keyboardType = UIKeyboardType.numberPad
     }
     
+}
+
+extension SettingVC : UITextFieldDelegate
+{
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
